1cf4b7ee62e1b57d1e3d9daf8d8311630233a4de
Devin Carraway <git@devin.com>
2025-11-03 21:02:13 -0800

    Cover more of mesh::Utils
    - printHex (demonstrates a mock/spy stream)
    - 2-part sha256

--- test/test_common/test_utils.cpp
+++ test/test_common/test_utils.cpp
@@ -1,8 +1,12 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <Stream.h>
+#include <string>
 
 #include "Utils.h"
 
 using namespace mesh;
+using ::testing::InSequence;
 
 
 TEST(UtilTests, NopTest)
@@ -16,6 +20,11 @@ TEST(UtilTests, SHA256)
   memset(hash, 0, sizeof(hash));
   uint8_t msg[] = "foo";
   mesh::Utils::sha256(hash, (size_t)sizeof(hash), msg, 3);
+  EXPECT_STREQ((char*)hash,
+			(char*)"\x2c\x26\xb4\x6b\x68\xff\xc6\x8f\xf9\x9b\x45\x3c\x1d\x30\x41\x34\x13\x42\x2d\x70\x64\x83\xbf\xa0\xf9\x8a\x5e\x88\x62\x66\xe7\xae");
+
+  memset(hash, 0, sizeof(hash));
+  mesh::Utils::sha256(hash, (size_t)sizeof(hash), msg, 1, msg+1, 2);
   EXPECT_STREQ((char*)hash,
 			(char*)"\x2c\x26\xb4\x6b\x68\xff\xc6\x8f\xf9\x9b\x45\x3c\x1d\x30\x41\x34\x13\x42\x2d\x70\x64\x83\xbf\xa0\xf9\x8a\x5e\x88\x62\x66\xe7\xae");
 }
@@ -81,11 +90,55 @@ TEST(UtilTests, parseTextParts)
   ASSERT_STREQ(parts[0], "b");
   ASSERT_STREQ(parts[1], "c");
 
+  strcpy(text, "d,,e");
+  ASSERT_EQ(mesh::Utils::parseTextParts(text, &parts[0], 10, ','), 3);
+  ASSERT_STREQ(parts[0], "d");
+  ASSERT_STREQ(parts[1], "");
+  ASSERT_STREQ(parts[2], "e");
+
   // This isn't normal string splitter behavior, but it's intentional
-  strcpy(text, "c,d,");
+  strcpy(text, "f,g,");
   ASSERT_EQ(mesh::Utils::parseTextParts(text, &parts[0], 10, ','), 2);
-  ASSERT_STREQ(parts[0], "c");
-  ASSERT_STREQ(parts[1], "d");
+  ASSERT_STREQ(parts[0], "f");
+  ASSERT_STREQ(parts[1], "g");
+}
+
+class MockStream : public Stream {
+public:
+  uint8_t *buffer;
+  size_t pos;
+  MockStream(uint8_t *b)
+  :buffer(b),pos(0)
+  {
+    buffer[0] = 0;
+  }
+
+  void clear() {
+    pos = 0;
+    buffer[0] = 0;
+  }
+
+  size_t write(uint8_t c) {
+    buffer[pos++] = c;
+    buffer[pos] = 0;
+    return 1;
+  }
+
+  MOCK_METHOD(int, available, (), (override));
+  MOCK_METHOD(size_t, write, (const uint8_t *buffer, size_t size), (override));
+  MOCK_METHOD(int, availableForWrite, (), (override));
+  MOCK_METHOD(int, read, (), (override));
+  MOCK_METHOD(int, peek, (), (override));
+};
+
+TEST(UtilTests, printHex)
+{
+  uint8_t out[10];
+  MockStream s(&out[0]);
+
+  const uint8_t src[] = "\x00\x7f\xab\xff";
+  mesh::Utils::printHex(s, src, 4);
+  EXPECT_STREQ((const char *)out, "007FABFF");
 }
 
 #if defined(ARDUINO)
