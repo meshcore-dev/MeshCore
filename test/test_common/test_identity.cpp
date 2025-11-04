9785cc6dd10c8669658d2c72c6379e7507562c09
Devin Carraway <git@devin.com>
2025-11-03 22:49:51 -0800

    mesh::LocalIdentity initializer tests

--- test/test_common/test_identity.cpp
+++ test/test_common/test_identity.cpp
@@ -3,6 +3,7 @@
 #include <Stream.h>
 #include <string>
 
+#include <ed_25519.h>
 #include "mock_streams.h"
 #include "Identity.h"
 
@@ -24,4 +25,22 @@ TEST(IdentityTests, Identity)
   MockStream bs(&buffer[0]);
   ASSERT_TRUE(id.writeTo(bs));
   ASSERT_STREQ((const char *)bs.buffer, (const char *)pubhex);
+}
+
+TEST(IdentityTests, LocalIdentity)
+{
+  uint8_t pub_key[PUB_KEY_SIZE], prv_key[PRV_KEY_SIZE], seed[SEED_SIZE];
+  memset(seed, 0, SEED_SIZE);
+  ed25519_create_keypair(pub_key, prv_key, seed);
+
+  uint8_t stored_key[PUB_KEY_SIZE+PRV_KEY_SIZE+SEED_SIZE];
+  memcpy(stored_key, pub_key, PUB_KEY_SIZE);
+  memcpy(stored_key+PUB_KEY_SIZE, prv_key, PRV_KEY_SIZE);
+  // we're not saving seeds yet
+  memset(stored_key+PUB_KEY_SIZE+PRV_KEY_SIZE, 0, SEED_SIZE);
+
+  ConstantValueStream skf(stored_key, sizeof(stored_key));
+  mesh::LocalIdentity id;
+  ASSERT_TRUE(id.readFrom(skf));
+
 }
