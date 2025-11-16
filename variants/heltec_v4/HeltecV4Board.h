d0c342fc14341f9bdac1c9f09ada3e2151b2a486
IoTThinks <vinhdat82@gmail.com>
2025-11-16 12:45:18 +0700

    To support powersaving mode with lightsleep at 9mA

--- variants/heltec_v4/HeltecV4Board.h
+++ variants/heltec_v4/HeltecV4Board.h
@@ -17,6 +17,8 @@ public:
   void onAfterTransmit(void) override;
   void enterDeepSleep(uint32_t secs, int pin_wake_btn = -1);
   void powerOff() override;
+  void enterLightSleep (uint32_t secs);
+  void sleep() override;
   uint16_t getBattMilliVolts() override;
   const char* getManufacturerName() const override ;
 
