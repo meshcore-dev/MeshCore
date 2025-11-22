ff3b84dce5ae8051a901cd3400a2906dd848e864
Ashley <webash@users.noreply.github.com>
2025-11-22 02:14:24 +0000

    Arduino Nesso N1: header/cpp rearrange + attempting to debug crash

--- variants/arduino_nesso_n1/ArduinoNessoN1Board.h
+++ variants/arduino_nesso_n1/ArduinoNessoN1Board.h
@@ -1,32 +1,20 @@
 #pragma once
 
 #include <Arduino.h>
+#include <MeshCore.h>
 #include <helpers/ESP32Board.h>
 #include "pins_arduino.h"
 
-#define P_LORA_TX_LED LED_BUILTIN
+#define P_LORA_TX_LED LED_BUILTIN // defined in pins_arduino.h / expander.cpp through pin handling functions specific to the IO expander
 // #define PIN_TFT_RST LCD_RESET 
 // #define PIN_TFT_LEDA_CTL LCD_BACKLIGHT
 
 class ArduinoNessoN1Board : public ESP32Board {
 private:
   NessoBattery battery;
+
 public:
-  void begin() {
-    ESP32Board::begin();
-#ifdef MESH_DEBUG
-    // delay for 2s after boot to ensure early output below makes it to the serial logger
-    delay(2000);
-#endif
-
-#ifdef P_LORA_TX_LED
-    MESH_DEBUG_PRINTLN("ArduinoNessoN1.begin(): setup TX LED mode");
-    pinMode(P_LORA_TX_LED, OUTPUT);
-    digitalWrite(P_LORA_TX_LED, HIGH);
-#endif
-
-    battery.enableCharge();
-  }
+  void begin(); // Defined in ArduinoNessoN1Board.cpp
 
 #ifdef P_LORA_TX_LED
   void onBeforeTransmit() override {
@@ -45,6 +33,11 @@ public:
 
   uint16_t getBattMilliVolts() override {
     return battery.getMilliVoltage();
+  }
+
+  void reboot() override {
+    MESH_DEBUG_PRINTLN("ArduinoNessoN1.reboot(): noop() instead");
+    // esp_restart();
   }
 };
 
