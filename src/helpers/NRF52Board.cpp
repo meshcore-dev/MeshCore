d9a02b2b96674e303c5d27ecb6ba1b7f2883c669
IoTThinks <vinhdat82@gmail.com>
2025-12-11 22:56:57 +0700

    To wait 1ms only to get temperature from NRF52 MCU

--- src/helpers/NRF52Board.cpp
+++ src/helpers/NRF52Board.cpp
@@ -1,17 +1,25 @@
 #if defined(NRF52_PLATFORM)
 #include "NRF52Board.h"
 
-// The default is from the built-in temperature sensor of MCU
+// Temperature from NRF52 MCU
 float NRF52Board::getMCUTemperature() {
   NRF_TEMP->TASKS_START = 1; // Start temperature measurement
 
-  while (NRF_TEMP->EVENTS_DATARDY == 0) { } // Wait for completion
+  long startTime = millis();  
+  while (NRF_TEMP->EVENTS_DATARDY == 0) { // Wait for completion. Should complete in 50us
+    if(millis() - startTime > 1) {  // To wait 1ms
+      NRF_TEMP->TASKS_STOP = 1;
+      return NAN;
+    }
+  }
+  
   NRF_TEMP->EVENTS_DATARDY = 0; // Clear event flag
 
-  int32_t temp = NRF_TEMP->TEMP; // In 0.25°C units
+  int32_t temp = NRF_TEMP->TEMP; // In 0.25 *C units
   NRF_TEMP->TASKS_STOP = 1;
 
-  return temp * 0.25f; // Convert to °C
+  return temp * 0.25f; // Convert to *C
 }
 
 #endif
+
