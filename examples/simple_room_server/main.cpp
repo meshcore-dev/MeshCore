14f00fe688a3586af6e5598b1b512698003e8920
João Brázio <jbrazio@gmail.com>
2025-12-14 01:12:38 +0000

    Update advertisement condition to include NO_BOOT_ADVERT check

--- examples/simple_room_server/main.cpp
+++ examples/simple_room_server/main.cpp
@@ -76,7 +76,7 @@ void setup() {
   ui_task.begin(the_mesh.getNodePrefs(), FIRMWARE_BUILD_DATE, FIRMWARE_VERSION);
 #endif
 
-#if !defined(STEALTH_MODE)
+#if !defined(STEALTH_MODE) && !defined(NO_BOOT_ADVERT)
   // send out initial Zero Hop Advertisement to the mesh
   the_mesh.sendSelfAdvertisement(16000, false);
 #endif
