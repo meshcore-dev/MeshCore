03ca28efb2187cf50898be85d8f51991ae174049
Kevin Le <vinhdat82@gmail.com>
2025-12-27 15:24:23 +0700

    Typo

--- examples/simple_repeater/MyMesh.cpp
+++ examples/simple_repeater/MyMesh.cpp
@@ -180,7 +180,7 @@ int MyMesh::handleRequest(ClientInfo *sender, uint32_t sender_timestamp, uint8_t
     }
     sensors.querySensors(perm_mask, telemetry);
 
-	// This default temperature will be overriden by external sensors (if any)
+	// This default temperature will be overridden by external sensors (if any)
     float temperature = board.getMCUTemperature();
     if(!isnan(temperature)) { // Supported boards with built-in temperature sensor. ESP32-C3 may return NAN
       telemetry.addTemperature(TELEM_CHANNEL_SELF, temperature); // Built-in MCU Temperature
