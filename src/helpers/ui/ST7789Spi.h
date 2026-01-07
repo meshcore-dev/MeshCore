7f6fea3cfeada8a9e8effded9b80621d727ec05e
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-01-07 01:08:58 -0500

    spelling: safely
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/helpers/ui/ST7789Spi.h
+++ src/helpers/ui/ST7789Spi.h
@@ -179,7 +179,7 @@ class ST7789Spi : public OLEDDisplay {
        }
 
        // If the minBoundY wasn't updated
-       // we can savely assume that buffer_back[pos] == buffer[pos]
+       // we can safely assume that buffer_back[pos] == buffer[pos]
        // holds true for all values of pos
        if (minBoundY == UINT16_MAX) return;
 
