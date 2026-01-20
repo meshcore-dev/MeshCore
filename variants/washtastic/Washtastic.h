582a5221ac654a119f9f4a737eac80fadd344f58
Kevin Le <vinhdat82@gmail.com>
2026-01-20 15:29:18 +0700

    Added support for Washtastic boards

---
+++ variants/washtastic/Washtastic.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <MeshCore.h>
+#include <Arduino.h>
+#include <helpers/NRF52Board.h>
+
+#define  PIN_VBAT_READ 17
+#define  ADC_MULTIPLIER   (1.815f) // dependent on voltage divider resistors. TODO: more accurate battery tracking
+
+class Washtastic : public NRF52Board {
+protected:
+  uint8_t startup_reason;
+  uint8_t btn_prev_state;
+  float adc_mult = ADC_MULTIPLIER;
+
+public:
+  void begin();
+
+  uint8_t getStartupReason() const override { return startup_reason; }
+
+  #define BATTERY_SAMPLES 8
+
+  uint16_t getBattMilliVolts() override {
+    analogReadResolution(12);
+
+    uint32_t raw = 0;
+    for (int i = 0; i < BATTERY_SAMPLES; i++) {
+      raw += analogRead(PIN_VBAT_READ);
+    }
+    raw = raw / BATTERY_SAMPLES;
+    return (adc_mult * raw);
+  }
+
+  bool setAdcMultiplier(float multiplier) override {
+    if (multiplier == 0.0f) {
+      adc_mult = ADC_MULTIPLIER;}
+    else {
+      adc_mult = multiplier;
+    }
+    return true;
+  }
+  float getAdcMultiplier() const override {
+    if (adc_mult == 0.0f) {
+      return ADC_MULTIPLIER;
+    } else {
+      return adc_mult;
+    }
+  }
+
+  const char* getManufacturerName() const override {
+    return "Washtastic";
+  }
+
+  int buttonStateChanged() {
+    #ifdef BUTTON_PIN
+      uint8_t v = digitalRead(BUTTON_PIN);
+      if (v != btn_prev_state) {
+        btn_prev_state = v;
+        return (v == LOW) ? 1 : -1;
+      }
+    #endif
+      return 0;
+  }
+
+  void reboot() override {
+    NVIC_SystemReset();
+  }
+  
+  void powerOff() override {
+    sd_power_system_off();
+  }
+
+  bool startOTAUpdate(const char* id, char reply[]) override;
+};
