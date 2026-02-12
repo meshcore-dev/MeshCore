4e319a6846165b403241ca45b98420615b5fb4e7
Confi <9595028+Confituurke@users.noreply.github.com>
2026-02-12 21:43:18 +0100

    sensecap_solar: use GPS_BAUD_RATE only, remove unused GPS_BAUDRATE

--- variants/sensecap_solar/variant.h
+++ variants/sensecap_solar/variant.h
@@ -64,8 +64,7 @@
 #define PIN_WIRE_SCL            (15)
 
 // GPS L76KB
-#define GPS_BAUDRATE            9600
-#define GPS_BAUD_RATE            GPS_BAUDRATE
+#define GPS_BAUD_RATE           9600
 #define GPS_THREAD_INTERVAL     50
 #define PIN_GPS_TX              PIN_SERIAL1_RX
 #define PIN_GPS_RX              PIN_SERIAL1_TX
