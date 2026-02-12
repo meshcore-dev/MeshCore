9ccf38d6bc1f16d78f67c97e3d82db6ee1a2cf6b
Reid Tonking <Reid.Tonking@gmail.com>
2026-02-12 13:09:30 -0600

    wio-tracker-l1: add power management support
    - Add boot voltage protection (won't boot below 3.3V)
    - Add LPCOMP wake configuration for voltage recovery
    - Implement initiateShutdown() with LPCOMP wake on low voltage
    - Enable NRF52_POWER_MANAGEMENT build flag
    
    Device will enter protective shutdown on low battery and wake
    automatically when voltage recovers to ~3.7V or USB is connected.
    
    Signed-off-by: Reid Tonking <Reid.Tonking@gmail.com>

--- variants/wio-tracker-l1/WioTrackerL1Board.cpp
+++ variants/wio-tracker-l1/WioTrackerL1Board.cpp
@@ -3,16 +3,46 @@
 
 #include "WioTrackerL1Board.h"
 
+#ifdef NRF52_POWER_MANAGEMENT
+// Static configuration for power management
+// Values set in variant.h defines
+const PowerMgtConfig power_config = {
+  .lpcomp_ain_channel = PWRMGT_LPCOMP_AIN,
+  .lpcomp_refsel = PWRMGT_LPCOMP_REFSEL,
+  .voltage_bootlock = PWRMGT_VOLTAGE_BOOTLOCK
+};
+
+void WioTrackerL1Board::initiateShutdown(uint8_t reason) {
+  bool enable_lpcomp = (reason == SHUTDOWN_REASON_LOW_VOLTAGE ||
+                        reason == SHUTDOWN_REASON_BOOT_PROTECT);
+
+  pinMode(VBAT_ENABLE, OUTPUT);
+  digitalWrite(VBAT_ENABLE, enable_lpcomp ? LOW : HIGH);
+
+  if (enable_lpcomp) {
+    configureVoltageWake(power_config.lpcomp_ain_channel, power_config.lpcomp_refsel);
+  }
+
+  enterSystemOff(reason);
+}
+#endif // NRF52_POWER_MANAGEMENT
+
 void WioTrackerL1Board::begin() {
   NRF52BoardDCDC::begin();
   btn_prev_state = HIGH;
 
-  // Configure battery voltage ADC
+  // Configure battery voltage ADC (needed for boot voltage check)
   pinMode(PIN_VBAT_READ, INPUT);
   pinMode(VBAT_ENABLE, OUTPUT);
   digitalWrite(VBAT_ENABLE, HIGH);        // Disable VBAT divider to save power
   analogReadResolution(12);
   analogReference(AR_INTERNAL);
+
+#ifdef NRF52_POWER_MANAGEMENT
+  // Boot voltage protection check (may not return if voltage too low)
+  checkBootVoltage(&power_config);
+#endif
+
   // Set all button pins to INPUT_PULLUP
   pinMode(PIN_BUTTON1, INPUT_PULLUP);
   pinMode(PIN_BUTTON2, INPUT_PULLUP);
@@ -20,7 +50,6 @@ void WioTrackerL1Board::begin() {
   pinMode(PIN_BUTTON4, INPUT_PULLUP);
   pinMode(PIN_BUTTON5, INPUT_PULLUP);
   pinMode(PIN_BUTTON6, INPUT_PULLUP);
-  
 
   #if defined(PIN_WIRE_SDA) && defined(PIN_WIRE_SCL)
     Wire.setPins(PIN_WIRE_SDA, PIN_WIRE_SCL);
--- variants/wio-tracker-l1/WioTrackerL1Board.h
+++ variants/wio-tracker-l1/WioTrackerL1Board.h
@@ -8,6 +8,10 @@ class WioTrackerL1Board : public NRF52BoardDCDC {
 protected:
   uint8_t btn_prev_state;
 
+#ifdef NRF52_POWER_MANAGEMENT
+  void initiateShutdown(uint8_t reason) override;
+#endif
+
 public:
   WioTrackerL1Board() : NRF52Board("WioTrackerL1 OTA") {}
   void begin();
