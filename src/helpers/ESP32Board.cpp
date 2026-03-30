ba3d8ba03030acc613d993bc8f0a9d1a3030f106
txkbaldlaw <txkbaldlaw@gmail.com>
2026-03-29 23:48:51 -0500

    WIP backup of ESP32 companion OTA investigation

--- src/helpers/ESP32Board.cpp
+++ src/helpers/ESP32Board.cpp
@@ -2,7 +2,7 @@
 
 #include "ESP32Board.h"
 
-#if defined(ADMIN_PASSWORD) && !defined(DISABLE_WIFI_OTA)   // Repeater or Room Server only
+#if (defined(ADMIN_PASSWORD) || defined(ENABLE_COMPANION_WIFI_OTA)) && !defined(DISABLE_WIFI_OTA)
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
@@ -12,20 +12,37 @@
 
 bool ESP32Board::startOTAUpdate(const char* id, char reply[]) {
   inhibit_sleep = true;   // prevent sleep during OTA
-  WiFi.softAP("MeshCore-OTA", NULL);
+  WiFi.persistent(false);
+  WiFi.softAPdisconnect(true);
+  WiFi.mode(WIFI_AP);
 
-  sprintf(reply, "Started: http://%s/update", WiFi.softAPIP().toString().c_str());
+  bool ap_ok = WiFi.softAP("MeshCore-OTA", NULL);
+  MESH_DEBUG_PRINTLN("startOTAUpdate: softAP returned %d", ap_ok);
+  if (!ap_ok) {
+    strcpy(reply, "Failed to start WiFi AP");
+    return false;
+  }
+
+  // Give the AP stack a moment to settle before starting the web server.
+  delay(250);
+
+  IPAddress ap_ip = WiFi.softAPIP();
+  MESH_DEBUG_PRINTLN("startOTAUpdate: softAPIP=%s", ap_ip.toString().c_str());
+  sprintf(reply, "Started: http://%s/update", ap_ip.toString().c_str());
   MESH_DEBUG_PRINTLN("startOTAUpdate: %s", reply);
 
   static char id_buf[60];
   sprintf(id_buf, "%s (%s)", id, getManufacturerName());
   static char home_buf[90];
-  sprintf(home_buf, "<H2>Hi! I am a MeshCore Repeater. ID: %s</H2>", id);
+  sprintf(home_buf, "<H2>Hi! I am a MeshCore node. ID: %s</H2>", id);
 
   AsyncWebServer* server = new AsyncWebServer(80);
 
   server->on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "text/html", home_buf);
+  });
+  server->on("/ping", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "OK");
   });
   server->on("/log", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(SPIFFS, "/packet_log", "text/plain");
