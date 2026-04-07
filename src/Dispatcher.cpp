05dd84151ab672bc7aadd191c9e1b133c15bbaff
overkillfpv <t.hitchins@exemail.com.au>
2026-04-07 21:23:30 +1000

    Increase CAD fail max duration from 6 seconds to 8 seconds for testing currently

--- src/Dispatcher.cpp
+++ src/Dispatcher.cpp
@@ -60,7 +60,7 @@ uint32_t Dispatcher::getCADFailRetryDelay() const {
   return 200;
 }
 uint32_t Dispatcher::getCADFailMaxDuration() const {
-  return 6000;   // 6 seconds
+  return 8000;   // 8 seconds
 }
 
 void Dispatcher::loop() {
