d5c9236533b5d248944b1db1276f7570bdc5f0cb
Rococo88 <michal.tarkowski@outlook.com>
2026-05-07 08:42:16 +0200

    Implement WiFi auto-reconnect functionality

--- examples/companion_radio/main.cpp
+++ examples/companion_radio/main.cpp
@@ -195,6 +195,10 @@ void setup() {
 
 #ifdef WIFI_SSID
   board.setInhibitSleep(true);   // prevent sleep when WiFi is active
+  WiFi.setAutoReconnect(true);
+  WiFi.onEvent([](WiFiEvent_t event, WiFiEventInfo_t info){
+      WIFI_DEBUG_PRINTLN("WiFi disconnected. Forcing reconnect...");
+  });
   WiFi.begin(WIFI_SSID, WIFI_PWD);
   serial_interface.begin(TCP_PORT);
 #elif defined(BLE_PIN_CODE)
