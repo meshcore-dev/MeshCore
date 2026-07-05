c2697269f69a31bbf02cd02864c7d25aeb1cb337
Kevin Le <vinhdat82@gmail.com>
2026-07-05 22:14:08 +0700

    Fixed Poweroff for TImpulsePlusBoard

--- variants/lilygo_t_impulse_plus/TImpulsePlusBoard.h
+++ variants/lilygo_t_impulse_plus/TImpulsePlusBoard.h
@@ -48,12 +48,10 @@ public:
   }
 
   void powerOff() override {
-
-    // turn off 3.3v
-    digitalWrite(RT9080_EN, LOW);
-
     // power off system
     NRF52Board::powerOff();
 
+    // turn off 3.3v
+    digitalWrite(RT9080_EN, LOW);
   }
 };
