b584909f32d8d7a2642d47a5abd574e10e374891
Gerard Hickey <hickey@kinetic-compute.com>
2026-07-21 14:42:00 -0400

    WIFI configured for examples/simple_repeater
    Signed-off-by: Gerard Hickey <hickey@kinetic-compute.com>

--- examples/simple_repeater/main.cpp
+++ examples/simple_repeater/main.cpp
@@ -35,6 +35,12 @@ static unsigned long userBtnDownAt = 0;
 #define USER_BTN_HOLD_OFF_MILLIS 1500
 #endif
 
+#ifdef ESP32
+  #ifdef WIFI_SSID
+    #include <helpers/esp32/SerialWifiInterface.h>
+  #endif
+#endif
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
@@ -93,6 +99,11 @@ void setup() {
     }
     store.save("_main", the_mesh.self_id);
   }
+
+#ifdef WIFI_SSID
+  board.setInhibitSleep(true);   // prevent sleep when WiFi is active
+  WiFi.begin(WIFI_SSID, WIFI_PWD);
+#endif
 
   Serial.print("Repeater ID: ");
   mesh::Utils::printHex(Serial, the_mesh.self_id.pub_key, PUB_KEY_SIZE); Serial.println();
