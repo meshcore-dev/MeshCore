6547959cd32bf2fcab41895ac2a2b4e257b1b67c
Scott Powell <ripple_biz@protonmail.com>
2026-08-09 15:14:46 +1000

    * version 1.17.0

--- examples/simple_repeater/MyMesh.h
+++ examples/simple_repeater/MyMesh.h
@@ -111,11 +111,11 @@ struct AttachedClient {
 };
 
 #ifndef FIRMWARE_BUILD_DATE
-  #define FIRMWARE_BUILD_DATE   "6 Jun 2026"
+  #define FIRMWARE_BUILD_DATE   "9 Aug 2026"
 #endif
 
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION   "v1.16.0"
+  #define FIRMWARE_VERSION   "v1.17.0"
 #endif
 
 #define FIRMWARE_ROLE "repeater"
