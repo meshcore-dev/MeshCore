919758cdf63ced83f8ca6642bc82f7078dde28fa
Daniel K <vk2trp@gmail.com>
2026-09-26 09:21:10 +1000

    Update variant for LR1121 changes

--- variants/muzi_base/target.cpp
+++ variants/muzi_base/target.cpp
@@ -77,83 +77,47 @@ void MuziBaseSensorManager::loop() {
   MomentaryButton back_btn(PIN_BACK_BTN, 1000, true, false, true);
 #endif
 
-#if defined(USE_LR1121)
-  #ifndef LORA_CR
-    #define LORA_CR      5
-  #endif
 
-  #ifdef RF_SWITCH_TABLE
-  static const uint32_t rfswitch_dios[Module::RFSWITCH_MAX_PINS] = {
-    RADIOLIB_LR11X0_DIO5,
-    RADIOLIB_LR11X0_DIO6,
-    RADIOLIB_NC
-  };
+#ifdef RF_SWITCH_TABLE
+static const uint32_t rfswitch_dios[Module::RFSWITCH_MAX_PINS] = {
+  RADIOLIB_LR11X0_DIO5,
+  RADIOLIB_LR11X0_DIO6,
+  RADIOLIB_NC
+};
 
-  static const Module::RfSwitchMode_t rfswitch_table[] = {
-    // mode                 DIO5  DIO6
-    { LR11x0::MODE_STBY,   {LOW,  LOW}},
-    { LR11x0::MODE_RX,     {HIGH, LOW}},
-    { LR11x0::MODE_TX,     {LOW,  HIGH}},
-    { LR11x0::MODE_TX_HP,  {LOW,  HIGH}},
-    { LR11x0::MODE_TX_HF,  {LOW,  LOW}},
-    { LR11x0::MODE_GNSS,   {LOW,  LOW}},
-    { LR11x0::MODE_WIFI,   {LOW,  LOW}},
-    END_OF_MODE_TABLE,
-  };
-  #endif
-#endif // USE_LR1121
+static const Module::RfSwitchMode_t rfswitch_table[] = {
+  // mode                 DIO5  DIO6
+  { LR11x0::MODE_STBY,   {LOW,  LOW}},
+  { LR11x0::MODE_RX,     {HIGH, LOW}},
+  { LR11x0::MODE_TX,     {LOW,  HIGH}},
+  { LR11x0::MODE_TX_HP,  {LOW,  HIGH}},
+  { LR11x0::MODE_TX_HF,  {LOW,  LOW}},
+  { LR11x0::MODE_GNSS,   {LOW,  LOW}},
+  { LR11x0::MODE_WIFI,   {LOW,  LOW}},
+  END_OF_MODE_TABLE,
+};
+#endif
 
 bool radio_init() {
   //rtc_clock.begin(Wire);
 
-#if defined(USE_LR1121)
-  #ifdef LR11X0_DIO3_TCXO_VOLTAGE
-    float tcxo = LR11X0_DIO3_TCXO_VOLTAGE;
-  #else
-    float tcxo = 1.6f;
-  #endif
-
-  SPI.setPins(P_LORA_MISO, P_LORA_SCLK, P_LORA_MOSI);
-  SPI.begin();
-  int status = radio.begin(LORA_FREQ, LORA_BW, LORA_SF, LORA_CR, RADIOLIB_LR11X0_LORA_SYNC_WORD_PRIVATE, LORA_TX_POWER, 16, tcxo);
-  if (status != RADIOLIB_ERR_NONE) {
-    Serial.print("ERROR: radio init failed: ");
-    Serial.println(status);
-    return false;  // fail
-  }
-
-  radio.setCRC(2);
-  radio.explicitHeader();
-
-  #ifdef RF_SWITCH_TABLE
-  radio.setRfSwitchTable(rfswitch_dios, rfswitch_table);
-  #endif
-  #ifdef RX_BOOSTED_GAIN
-  radio.setRxBoostedGainMode(RX_BOOSTED_GAIN);
-  #endif
-
-  return true;  // success
-#else  // USE_SX1262
-  // CustomSX1262::std_init() configures the SPI pins, runs begin() with the
-  // TCXO-voltage fallback, sets CRC, current limit, the DIO2 RF switch, and
-  // RX boosted gain from the SX126X_* build flags.
-  return radio.std_init(&SPI);
+#if defined(P_LORA_SCLK)
+  int err = radio.std_init(&SPI);
+  if (err != 1) return err;
+#else
+  int err = radio.std_init();
+  if (err != 1) return err;
 #endif
+
+#ifdef RF_SWITCH_TABLE
+  radio.setRfSwitchTable(rfswitch_dios, rfswitch_table);
+#endif
+
+  return true;
 }
 
 uint32_t radio_get_rng_seed() {
   return radio.random(0x7FFFFFFF);
-}
-
-void radio_set_params(float freq, float bw, uint8_t sf, uint8_t cr) {
-  radio.setFrequency(freq);
-  radio.setSpreadingFactor(sf);
-  radio.setBandwidth(bw);
-  radio.setCodingRate(cr);
-}
-
-void radio_set_tx_power(int8_t dbm) {
-  radio.setOutputPower(dbm);
 }
 
 mesh::LocalIdentity radio_new_identity() {
